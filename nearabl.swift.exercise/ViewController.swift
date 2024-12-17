cbc18a1453b6a77c7ec6246da0d109b63a25f185
jinchen003 <89657431+jinchen003@users.noreply.github.com>
2024-12-17 17:10:05 -0500

    init repo

---
+++ nearabl.swift.exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nearabl.swift.exercise
+//
+//  Created by Jin Chen on 12/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
